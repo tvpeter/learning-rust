902b412ff8cb10f803b1cc4ccee14b91f4318102
tvpeter <withtvpeter@gmail.com>
2022-10-03 12:59:07 +0100

    add custom error types activity

---
+++ src/bin/a27_custom_errors.rs
@@ -0,0 +1,64 @@
+/*
+    Topic: Custom Eorr Types
+    
+    Requirements:
+    - modify the 'ProgramError' enum in order to make the program compile and run.
+    Do not modify any other part of the program.
+    - The output should display a menu error followed by a math error when running.
+
+    Notes:
+    - use '#[error("description")]' on the enum variants
+    - use '#[from] ErrorType' to convert the existing errors into a 'ProgramError'
+ */
+
+ use thiserror::Error;
+
+ #[derive(Debug, Error)]
+ enum ProgramError {
+    #[error("Math error")]
+    Math(#[from] MathError),
+    #[error("Menu error")]
+    Menu(#[from] MenuError),
+ }
+
+ #[derive(Debug, Error)]
+ enum MenuError {
+    #[error("menu item not found")]
+    NotFound,
+ }
+
+ #[derive(Debug, Error)]
+ enum MathError {
+    #[error("divide by zero error")]
+    DivideByZero,
+ }
+
+ fn pick_menu(choice: &str) -> Result<i32, MenuError> {
+    match choice {
+        "1" => Ok(1),
+        "2" => Ok(2),
+        "3" => Ok(3),
+        _ => Err(MenuError::NotFound),
+    }
+ }
+
+ fn divide( a: i32, b: i32) -> Result<i32, MathError> {
+    if b!= 0 {
+        Ok(a/b)
+    } else {
+        Err(MathError::DivideByZero)
+    }
+ }
+
+ fn run (step: i32) -> Result<(), ProgramError> {
+    if step == 1 {
+        pick_menu("4")?;
+    } else if step == 2 {
+        divide(1, 0)?;
+    }
+    Ok(())
+ }
+ fn main(){
+    println!("{:?}", run(1));
+    println!("{:?}", run(2));
+ }
